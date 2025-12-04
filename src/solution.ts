f175e0099d28536bccaeeb8c8ef394afd37e5d51
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-05 04:15:35 +0600

    total 4 problem solved

--- src/solution.ts
+++ src/solution.ts
@@ -30,10 +30,3 @@ interface Item {
 const filterByRating = (items: Item[]): Item[] => {
   return items.filter((item) => item.rating >= 4);
 };
-const books = [
-  { title: "Book A", rating: 4.5 },
-  { title: "Book B", rating: 3.2 },
-  { title: "Book C", rating: 5.0 },
-];
-
-console.log(filterByRating(books));
