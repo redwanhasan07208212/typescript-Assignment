c8d526dec481f658fe19a5b8ab0c70c64ef9509e
Redwan <76751002+redwanhasan07208212@users.noreply.github.com>
2025-12-05 03:50:20 +0600

    1st problem solved

---
+++ src/solution.ts
@@ -0,0 +1,11 @@
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+};
